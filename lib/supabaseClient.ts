eeb4c1a5e1d2acd16dbd894e219073c90f7714ae
Simon <simoncollard8@gmail.com>
2026-02-17 19:14:17 -0600

    feat: Victorian redesign complete

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,6 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
-
